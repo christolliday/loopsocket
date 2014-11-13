b72c5ec52753e36eb06fb29cf7a47d667a6b315e
Chris Tolliday <ctolliday@gmail.com>
2014-11-12 18:03:45 -0800

    removed sessions module, fixed references in loops

--- loopSocket/public/modules/loops/controllers/playback.client.controller.js
+++ loopSocket/public/modules/loops/controllers/playback.client.controller.js
@@ -1,11 +1,11 @@
 'use strict';
 
 // Sessions controller
-angular.module('sessions').controller('PlaybackController', ['$scope',
+angular.module('loops').controller('PlaybackController', ['$scope',
 	function($scope) {
 
-		var time = 0;
-		var audio = new Audio('samples/hi-hat-closed.wav');
+		$scope.time = 0;
+		//var audio = new Audio('samples/hi-hat-closed.wav');
 
 		// Create new Session
 		$scope.play = function() {
@@ -17,26 +17,26 @@ angular.module('sessions').controller('PlaybackController', ['$scope',
 
 		function update_clock() {
 			//$scope.timeModel[time] = true;
-			console.log(time);
-			if($scope.hi_hat[time]) {
+			console.log($scope.time);
+			if($scope.hi_hat[$scope.time]) {
 				audio.play();
 			}
 
 			for(var i=0;i<8;i++) {
 				$scope.time_model[i] = false;
 			}
-			$scope.time_model[time] = true;
-			time = (time+1)%8;
+			$scope.time_model[$scope.time] = true;
+			$scope.time = ($scope.time+1)%8;
 			if($scope.playing) {
 				setTimeout(update_clock, 1000);
 			}
-		}
+		};
 		$scope.sample_click = function(index) {
 			$scope.hi_hat[index] = true;
-		}
+		};
 		function enable_beat() {
 
-		}
+		};
 		$scope.beats = [];
 		for(var i=0;i<8;i++) {
 			$scope.beats[i] = i+1;
@@ -44,14 +44,14 @@ angular.module('sessions').controller('PlaybackController', ['$scope',
 		$scope.hi_hat = [];
 		$scope.time_model = [];
 		$scope.playing = false;
-		/*$scope.hihatModel = [];
+		$scope.hihatModel = [];
 		for(var i=0;i<8;i++) {
 			$scope.hihatModel[i] = false;
 		}
 		$scope.timeModel = [];
 		for(var i=0;i<8;i++) {
 			$scope.timeModel[i] = false;
-		}*/
+		}
 
 		$scope.toggle_play = function() {
 			var playBtn = $scope.getElementById("playBtn");
