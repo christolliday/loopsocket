1d3ca04ddd227e24ea31285b281e87e976c56688
Chris Tolliday <ctolliday@gmail.com>
2014-11-17 22:29:52 -0800

    fixed audio playback to use web audio api not 'Audio()', no longer has to wait for full sample to stop before starting the next one

--- loopSocket/public/modules/loops/controllers/playback.client.controller.js
+++ loopSocket/public/modules/loops/controllers/playback.client.controller.js
@@ -4,17 +4,46 @@
 
 // Sessions controller
 angular.module('loops').controller('PlaybackController', ['$scope', '$document',
-	function($scope,$document) {
+	function($scope, $document) {
 
+		var audioBuffer = null;
+		window.AudioContext = window.AudioContext || window.webkitAudioContext;
+		var context = new AudioContext();
+
+		function loadAudio(url) {
+			var request = new XMLHttpRequest();
+			request.open('GET', url, true);
+			request.responseType = 'arraybuffer';
+
+			// Decode asynchronously
+			request.onload = function() {
+				context.decodeAudioData(request.response, function(buffer) {
+					audioBuffer = buffer;
+				}, function(error) {console.log(error);}
+				);
+			}
+			request.send();
+		}
+		var context = new AudioContext();
+
+		function playSound(buffer) {
+			var source = context.createBufferSource(); // creates a sound source
+			source.buffer = audioBuffer; // tell the source which sound to play
+			source.connect(context.destination); // connect the source to the context's destination (the speakers)
+			source.start(0); // play the source now
+		}
+		loadAudio('samples/play/hi-hat-closed.wav');
 
 
 		$scope.time = 0;
-		var audio = new Audio('samples/play/hi-hat-closed.wav');
+		$scope.hi_hat = [];
+		$scope.playing = false;
+		//var audio = new Audio('samples/play/hi-hat-closed.wav');
 		var timeout;
 
 		// Create new Session
 		$scope.play = function() {
-			if(!$scope.playing) {
+			if (!$scope.playing) {
 				console.log("play");
 
 				playback();
@@ -23,45 +52,42 @@ angular.module('loops').controller('PlaybackController', ['$scope', '$document',
 				clearTimeout(timeout);
 			}
 		};
+
 		function time_tick() {
-			$scope.time = (($scope.time+1)%8);
+			$scope.time = (($scope.time + 1) % 8);
 		}
+
 		function playback() {
-			if($scope.hi_hat[$scope.time]) {
-				audio.play();
+			if ($scope.hi_hat[$scope.time]) {
+				//audio.play();
+				playSound(audioBuffer);
 			}
 		}
+
 		function update_clock() {
 
 			time_tick();
 			playback();
 			$scope.$apply();
-			if($scope.playing) {
+			if ($scope.playing) {
 				timeout = setTimeout(update_clock, 500);
 			}
 		};
 		$scope.sample_click = function(index) {
-			$scope.hi_hat[index] = true;
+			$scope.hi_hat[index] = !$scope.hi_hat[index];
 		};
-		function enable_beat() {
-
-		};
-
-		$scope.hi_hat = [];
-		$scope.playing = false;
-
 		$scope.at_time = function(i) {
-			return $scope.time==(i-1);
+			return $scope.time == (i - 1);
 		}
 	}
 ]);
 
 angular.module('loops').filter('range', function() {
-  return function(input, min, max) {
-    min = parseInt(min);
-    max = parseInt(max);
-    for (var i=min; i<=max; i++)
-      input.push(i);
-    return input;
-  };
+	return function(input, min, max) {
+		min = parseInt(min);
+		max = parseInt(max);
+		for (var i = min; i <= max; i++)
+			input.push(i);
+		return input;
+	};
 });
