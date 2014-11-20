0d467bf9489d4f3d3d4efd0a31a901250a0f6e53
Chris Tolliday <ctolliday@gmail.com>
2014-11-19 19:15:22 -0800

    added sample select to edit page 2

--- loopSocket/public/modules/loops/controllers/playback.client.controller.js
+++ loopSocket/public/modules/loops/controllers/playback.client.controller.js
@@ -3,8 +3,8 @@
 
 
 // Sessions controller
-angular.module('loops').controller('PlaybackController', ['$scope', '$document',
-	function($scope, $document) {
+angular.module('loops').controller('PlaybackController', ['$scope', '$document', 'Samples',
+	function($scope, $document, Samples) {
 
 		//var socket = io.connect('http://localhost:3000');
 		var socket = io();
@@ -19,6 +19,7 @@ angular.module('loops').controller('PlaybackController', ['$scope', '$document',
 		function loadAudio(url) {
 			var request = new XMLHttpRequest();
 			request.open('GET', url, true);
+			request.setRequestHeader ("Accept", "audio/wav");
 			request.responseType = 'arraybuffer';
 
 			// Decode asynchronously
@@ -27,10 +28,9 @@ angular.module('loops').controller('PlaybackController', ['$scope', '$document',
 					audioBuffer = buffer;
 				}, function(error) {console.log(error);}
 				);
-			}
+			};
 			request.send();
 		}
-		var context = new AudioContext();
 
 		function playSound(buffer) {
 			var source = context.createBufferSource(); // creates a sound source
@@ -51,12 +51,21 @@ angular.module('loops').controller('PlaybackController', ['$scope', '$document',
 		var timeout;
 		var index = 16;
 
+		$scope.samples = Samples.query(function() {
+			$scope.selectedSample = $scope.samples[0];
+		});
+		$scope.changeSample = function() {
+			var url = 'samples/'+$scope.selectedSample._id;
+			loadAudio(url);
+		};
+
 		// Create new Session
 		$scope.play = function() {
 			if (!$scope.playing) {
 				$scope.playing = true;
-				console.log("play");
+				
 				socket.emit('serverListner', 'playClicked');
+
 				playback();
 				timeout = setTimeout(update_clock, 500);
 			} else {
@@ -73,7 +82,7 @@ angular.module('loops').controller('PlaybackController', ['$scope', '$document',
 
 		$scope.clear = function() {
 
-		}
+		};
 
 		function time_tick() {
 			$scope.time = (($scope.time + 1) % index);
@@ -101,7 +110,7 @@ angular.module('loops').controller('PlaybackController', ['$scope', '$document',
 			if ($scope.playing) {
 				timeout = setTimeout(update_clock, 500);
 			}
-		};
+		}
 		$scope.sample_click = function(index,instrument) {
 			if(!$scope.instruments[instrument]) {
 				$scope.instruments[instrument] = [];
@@ -111,7 +120,7 @@ angular.module('loops').controller('PlaybackController', ['$scope', '$document',
 		};
 		$scope.at_time = function(i) {
 			return $scope.time == (i - 1);
-		}
+		};
 
 		$scope.pressed = function (index, instrument) {
 	
