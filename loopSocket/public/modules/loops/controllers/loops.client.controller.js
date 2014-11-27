3ab9966cfd175be91ac0d2e3acd82915c8736b2a
jcc66 <jcc66@sfu.ca>
2014-11-26 18:49:31 -0800

    fix edit loop

--- loopSocket/public/modules/loops/controllers/loops.client.controller.js
+++ loopSocket/public/modules/loops/controllers/loops.client.controller.js
@@ -76,44 +76,6 @@ angular.module('loops').controller('LoopsController', ['$scope', '$stateParams',
 			});
 		};
 
-		$scope.addPerson = function(index){
-			var loop = $scope.loop;
-			var length = $scope.loop.member.length;
-			loop.member[length] = userInfo[index]._id;
-			loop.$update(function() {
-				$location.path('loops/' + loop._id);
-			}, function(errorResponse) {
-				$scope.error = errorResponse.data.message;
-			});
-		};
-		$scope.checkPerson = function(index){
-			var loop = $scope.loop;
-			var length = $scope.loop.member.length;
-			for (var i=0; i<length; i++){
-				if (userInfo[index]._id === $scope.loop.member[i]){
-					return false;
-				}
-			}
-			if ($scope.loop.user._id === userInfo[index]._id){
-				return false;
-			}
-			else{
-				return true;
-			}
-		};
-		$scope.makePublic = function(){
-			var loop = $scope.loop;
-			loop.member[0] = 'Public';
-			loop.$update(function() {
-				$location.path('loops/' + loop._id);
-			}, function(errorResponse) {
-				$scope.error = errorResponse.data.message;
-			});
-		};
-		$scope.makePrivate = function(){
-			var loop = $scope.loop;
-			loop.member[0] = 'Private';
-
 		// Update connected users
 		$scope.updateConUsers = function() {
 			var loop = $scope.loop;
@@ -138,45 +100,6 @@ angular.module('loops').controller('LoopsController', ['$scope', '$stateParams',
 			}, function(errorResponse) {
 				$scope.error = errorResponse.data.message;
 			});
-		};
-		$scope.isPublic = function(){
-			var loop = $scope.loop;
-			if (loop.member[0] === 'Public'){
-				return false;
-			}
-			else{
-				return true;
-			}
-
-		};
-		$scope.isPrivate = function(){
-			var loop = $scope.loop;
-			if (loop.member[0] === 'Private'){
-				return false;
-			}
-			else{
-				return true;
-			}
-
-		};
-
-		$scope.checkList = function(index){
-			var list = $scope.listMembers[index];
-			var length = $scope.listMembers[index].member.length;
-			if ($scope.listMembers[index].member[0] == 'Public'){
-				console.log('1');
-				return false;
-			}
-			else{
-				if ($scope.listMembers[index].user._id == $scope.user._id){
-					console.log('2');
-					return false;
-				}
-				else{
-					return true;
-				}
-			}
-		};
 			sendState();
 		};
 
