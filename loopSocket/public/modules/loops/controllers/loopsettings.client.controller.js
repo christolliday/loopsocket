52e4fbe7477197cfacf1700213451f341055039e
Chris Tolliday <ctolliday@gmail.com>
2014-11-30 02:56:57 -0800

    Cleaned up loop settings code

--- loopSocket/public/modules/loops/controllers/loopsettings.client.controller.js
+++ loopSocket/public/modules/loops/controllers/loopsettings.client.controller.js
@@ -1,53 +1,42 @@
 'use strict';
 
 // Loops controller
-angular.module('loops').controller('LoopSettingsController', ['$scope', '$stateParams', '$location', 'Authentication', 'Loops', '$http',
-	function($scope, $stateParams, $location, Authentication, Loops, $http) {
+angular.module('loops').controller('LoopSettingsController', ['$scope', '$stateParams', '$location', 'Authentication', 'Loops', '$http', 'Users',
+	function($scope, $stateParams, $location, Authentication, Loops, $http, Users) {
 		$scope.authentication = Authentication;
 
 		$scope.loop = Loops.get({
 				loopId: $stateParams.loopId
 			});
 
-		var userInfo;
+		$scope.users = Users.query();
 
-		$scope.information = {};
-		$http.get('../../users').success(function(data){
-			$scope.information = data;
-			userInfo = data;
-		});
+		$scope.getUsers = function(index){
+			return $scope.users;
+		}
 
-		$scope.listMembers = {};
-		$http.get('/members').success(function(data){
-			$scope.listMembers = data;
-		});
-
-		$scope.addPerson = function(index){
+		$scope.addMember = function(user){
 			var loop = $scope.loop;
 			var length = $scope.loop.member.length;
-			loop.member[length] = userInfo[index]._id;
+			loop.member[length] = user._id;
 			loop.$update(function() {
 				$location.path('loops/' + loop._id);
 			}, function(errorResponse) {
 				$scope.error = errorResponse.data.message;
 			});
 		};
-		$scope.checkPerson = function(index){
-
+		$scope.isMember = function(index){
 			if($scope.loop.$resolved) {
-				var length = $scope.loop.member.length;
-				for (var i=0; i<length; i++){
-					if (userInfo[index]._id === $scope.loop.member[i]){
+				for (var i=0; i<$scope.loop.member.length; i++){
+					if ($scope.users[index]._id === $scope.loop.member[i]){
 						return false;
 					}
 				}
-				if ($scope.loop.user._id === userInfo[index]._id){
+				if ($scope.loop.user._id === $scope.users[index]._id){
 					return false;
 				}
-				else{
-					return true;
-				}
 			}
+			return true;
 		};
 		$scope.makePublic = function(){
 			var loop = $scope.loop;
@@ -69,27 +58,15 @@ angular.module('loops').controller('LoopSettingsController', ['$scope', '$stateP
 		};
 
 		$scope.isPublic = function(){
-			var loop = $scope.loop;
-			if (loop.permission_mode === 'Public'){
-				return false;
-			}
-			else{
-				return true;
-			}
+			return $scope.loop.permission_mode === 'Public';
 		};
 		$scope.isPrivate = function(){
-			var loop = $scope.loop;
-			if (loop.permission_mode === 'Private'){
-				return false;
-			}
-			else{
-				return true;
-			}
-
+			return $scope.loop.permission_mode === 'Private';
 		};
 
+		// ???
 		$scope.checkList = function(index){
-			var list = $scope.listMembers[index];
+			/*var list = $scope.listMembers[index];
 			var length = $scope.listMembers[index].member.length;
 			if ($scope.listMembers[index].permission_mode === 'Public'){
 				return false;
@@ -101,7 +78,7 @@ angular.module('loops').controller('LoopSettingsController', ['$scope', '$stateP
 				else{
 					return true;
 				}
-			}
+			}*/
 		};
 	}
 ]);
