c5b11bddf6c2824b68ede5cfe9a07db4f75f1529
udinkar <udinkar@sfu.ca>
2014-11-30 22:17:00 -0800

    Change port for production

--- loopSocket/config/env/production.js
+++ loopSocket/config/env/production.js
@@ -2,7 +2,7 @@
 
 module.exports = {
 	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/loopsocket',
-	port: 3100,
+	port: 80,
 	baseUrl: 'loopsocket',
 	assets: {
 		lib: {
